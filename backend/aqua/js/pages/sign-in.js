bf6e582bb0346ffedf7f954c9230e6e8013e380c
sthWrong <github@sthrong.com>
2015-10-13 20:24:13 +0800

    add screenshots

--- backend/aqua/js/pages/sign-in.js
+++
@@ -1,7 +0,0 @@
-/* Forgot Password Toggle */
-$('#forgotPassword').on('show.bs.collapse', function () {
-	$('.close-forgot-password').show();
-})
-$('#forgotPassword').on('hide.bs.collapse', function () {
-	$('.close-forgot-password').hide();
-})
